63ae22408467db6c86628a0ef3bf3d80f26a7e54
CyberL1 <mateusz.hf18@gmail.com>
2025-01-14 15:01:20 -0500

    fix(server): expose container stats

---
+++ src/routes/containers/_id/stats.ts
@@ -0,0 +1,10 @@
+import type { Container } from "#src/types/Container.ts";
+import { getContainer } from "#src/utils/containers.ts";
+import type { FastifyInstance, FastifyRequest } from "fastify";
+
+export default (fastify: FastifyInstance) => {
+  fastify.get("/", (req: FastifyRequest<{ Params: Container }>) => {
+    const container = getContainer(req.params.id);
+    return container.stats();
+  });
+};
