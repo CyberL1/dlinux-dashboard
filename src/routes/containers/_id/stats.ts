f4161b4f470d640960b3ea880bbd1746436553a3
CyberL1 <mateusz.hf18@gmail.com>
2025-01-14 17:08:12 -0500

    fix(server): container stats sending

--- src/routes/containers/_id/stats.ts
+++ src/routes/containers/_id/stats.ts
@@ -3,8 +3,27 @@ import { getContainer } from "#src/utils/containers.ts";
 import type { FastifyInstance, FastifyRequest } from "fastify";
 
 export default (fastify: FastifyInstance) => {
-  fastify.get("/", (req: FastifyRequest<{ Params: Container }>) => {
-    const container = getContainer(req.params.id);
-    return container.stats();
-  });
+  fastify.get(
+    "/",
+    async (req: FastifyRequest<{ Params: Container }>, reply) => {
+      const container = getContainer(req.params.id);
+
+      reply.header("Content-Type", "text/event-stream");
+      reply.header("Cache-Control", "no-cache");
+      reply.header("Connection", "keep-alive");
+
+      const stream = await container.stats({ stream: true });
+
+      if (!stream) {
+        reply.status(500).send("Error fetching stats");
+        return;
+      }
+      stream.on("data", (chunk) => {
+        reply.raw.write(`data: ${chunk.toString()}\n\n`);
+      });
+
+      stream.on("end", () => reply.raw.end());
+      return stream;
+    },
+  );
 };
